8f4ddc910351bc9725ad9ed1492d9f0b3fbe30b4
OhBlihv <dev@ohblihv.me>
2017-11-06 13:08:59 +0800

    Update vote Durability/Names for BB

--- SkyCore/Games/BuildBattle/Items/ItemVote.cs
+++ SkyCore/Games/BuildBattle/Items/ItemVote.cs
@@ -4,29 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using fNbt;
+using MiNET.Blocks;
 using MiNET.Items;
 
 namespace SkyCore.Games.BuildBattle.Items
 {
-	public class ItemVote : ItemMap //TODO: Change to ItemWool
+	public class ItemVote : ItemBlock
 	{
 
 		private readonly int _voteValue;
 		private readonly string _votePlayer;
 
-		public ItemVote(int voteValue, string votePlayer)
+		public ItemVote(int voteValue, string votePlayer) : base(new StainedGlassPane(), (short) GetVoteId(_getCorrectedVoteValue(voteValue)))
 		{
-			_voteValue = voteValue;
-			if (_voteValue < 1)
-			{
-				_voteValue = 1;
-			}
-			else if (_voteValue > 5)
-			{
-				_voteValue = 5;
-			}
-			
+			_voteValue = _getCorrectedVoteValue(voteValue);
+
 			_votePlayer = votePlayer;
+
+			Durability = GetVoteId(voteValue);
+		}
+
+		private static int _getCorrectedVoteValue(int voteValue)
+		{
+			if (voteValue < 1)
+			{
+				voteValue = 1;
+			}
+			else if (voteValue > 5)
+			{
+				voteValue = 5;
+			}
+
+			return voteValue;
 		}
 
 		private static readonly NbtCompound[] _extraData = new NbtCompound[6]; //1-5
@@ -47,7 +56,6 @@ namespace SkyCore.Games.BuildBattle.Items
 				{
 					new NbtCompound("display")
 					{
-						//new NbtString("Name", $"§r§d§lVote:§r {voteName}§r"),
 						new NbtString("Name", "§r"),
 						new NbtList("Lore")
 						{
@@ -61,25 +69,53 @@ namespace SkyCore.Games.BuildBattle.Items
 			return _extraData[_voteValue];
 		}
 
+		public static int GetVoteId(int i)
+		{
+			int damage;
+			switch (i)
+			{
+				case 1: //Terrible
+					damage = 14;
+					break;
+				case 2: //Poor
+					damage = 1;
+					break;
+				case 3: //Alright
+					damage = 13;
+					break;
+				case 4: //Great
+					damage = 3;
+					break;
+				case 5: //Amazing
+					damage = 2;
+					break;
+				default:
+					damage = 0;
+					break;
+			}
+
+			return damage;
+		}
+
 		public static string GetVoteName(int i)
 		{
 			string voteName;
 			switch (i)
 			{
 				case 1:
-					voteName = "§4Poop";
+					voteName = "§cTerrible";
 					break;
 				case 2:
-					voteName = "§cPoor";
+					voteName = "§6Poor";
 					break;
 				case 3:
-					voteName = "§6Alright";
+					voteName = "§2Alright";
 					break;
 				case 4:
-					voteName = "§eGood";
+					voteName = "§bGreat";
 					break;
 				case 5:
-					voteName = "§aAwesome!";
+					voteName = "§aAmazing";
 					break;
 				default:
 					voteName = "§dN/A";
