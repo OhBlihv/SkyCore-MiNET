9f54c489d00c4d3489b786730fec218ba463d7d0
OhBlihv <dev@ohblihv.me>
2017-11-24 22:22:08 +0800

    Attempt to detach from BugSnag to avoid unexpected errors inside BugSnag

--- SkyCore/BugSnag/BugSnagUtil.cs
+++ SkyCore/BugSnag/BugSnagUtil.cs
@@ -8,13 +8,13 @@ namespace SkyCore.BugSnag
 	public class BugSnagUtil
 	{
 
-		private static BaseClient _bugSnagClient;
+		//private static BaseClient _bugSnagClient;
 
-		private static readonly object BugSnagLock = new object();
+		//private static readonly object BugSnagLock = new object();
 
 		public static void Init()
 		{
-			_bugSnagClient = new BaseClient("189d7e1ed0d5c11045f91967f0f3f32b");
+			//_bugSnagClient = new BaseClient("189d7e1ed0d5c11045f91967f0f3f32b");
 			
 			/*lock (BugSnagLock)
 			{
@@ -29,14 +29,14 @@ namespace SkyCore.BugSnag
 			{
 				//Console.WriteLine(e);
 
-				Metadata metadata = new Metadata();
+				//Metadata metadata = new Metadata();
 
 				//metadatable?.PopulateMetadata(metadata);
 
-				lock (BugSnagLock)
+				/*lock (BugSnagLock)
 				{
 					_bugSnagClient.Notify(e, metadata);
-				}
+				}*/
 
 				//SkyUtil.log("Reported 1 Bug to BugSnag");
 			}
